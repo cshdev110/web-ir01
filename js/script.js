298aeb120f47f9457253528ec2199b2e146e2cc6
Dan <dan.devars8@gmail.com>
2023-05-31 21:10:47 +1000

    On footer.

--- js/script.js
+++ js/script.js
@@ -7,9 +7,19 @@ const btnSlideRightHeader = document.querySelector('#btn-slide-right-header');
 let slideHeader = 0;
 
 // nav vars
-// q: how can I do the responsive menu?
+const menuResponsive = document.querySelector('#evnt-menu-responsive');
+const containerMenuResponsive = document.querySelector('#container-elems-nav');
+const subMenuDropdown = document.querySelector('#dropdown-sub-menu');
+const subMenuDropdownContent = document.querySelector('#sub-menu');
 
+// section 2 vars
+const lunch_plates = document.querySelectorAll('.lunch-plates img');
 
+// section 4 vars
+const dinner_plates = document.querySelectorAll('.dinner-plates img');
+
+// section 5 vars
+const takeAway_bookings = document.querySelectorAll('.container-take-away-booking img');
 // ********************* End Variables *********************
 
 
@@ -31,7 +41,17 @@ function showSlideHeader(slide_number, slide_side = 'right'){
     });
 }
 
-// End Slide Header
+// menu responsive
+function showMenuResponsive(){
+    console.log(window.getComputedStyle(containerMenuResponsive, null).display);
+    if(window.getComputedStyle(containerMenuResponsive, null).display == 'none'){
+        containerMenuResponsive.style.display = 'flex';
+    }else{
+        containerMenuResponsive.style.display = '';
+    }
+}
+
+
 // ********************* End Functions *********************
 
 // ********************* Events *********************
@@ -54,7 +74,59 @@ btnSlideRightHeader.addEventListener('click', async () => {
     showSlideHeader(slideHeader, 'right');
 });
 
-// End Slide Header
+// menu responsive events
+menuResponsive.addEventListener('click', () => {
+    showMenuResponsive();
+
+});
+
+// sub menu dropdown. It adds the classes on the css file to show the sub menu for different screen sizes
+// when the screen is less than 768px or over. 
+// Ending removing the classes when the user clicks outside the sub menu to reset the styles
+document.addEventListener('click', (evt) => {
+    if(subMenuDropdown.contains(evt.target)){
+        subMenuDropdown.classList.toggle('parent-sub-menu-to-dropdown');
+        if(window.innerWidth <= 768){
+            subMenuDropdownContent.classList.toggle('active-sub-menu-media-screen');
+        }else{
+            subMenuDropdownContent.classList.toggle('active-sub-menu');
+        }
+    }else{
+        subMenuDropdown.classList.remove('parent-sub-menu-to-dropdown');
+        subMenuDropdownContent.classList.remove('active-sub-menu');
+        subMenuDropdownContent.classList.remove('active-sub-menu-media-screen');
+    }
+});
+
+// section 2 events - send to lunch menu page
+lunch_plates.forEach((plate) => {
+    plate.addEventListener('click', () => {
+        window.location.href = 'pages/lunch_menu.html';
+    });
+});
+
+// section 4 events - send to dinner menu page
+dinner_plates.forEach((plate) => {
+    plate.addEventListener('click', () => {
+        window.location.href = 'pages/dinner_menu.html';
+    });
+});
+
+// section 5 events - send to take away and booking page
+takeAway_bookings.forEach((elem, indx) => {
+    if(indx == 0){
+        elem.addEventListener('click', () => {
+            window.location.href = 'pages/take_away.html';
+        });
+    }else{
+        elem.addEventListener('click', () => {
+            window.location.href = 'pages/booking.html';
+        });
+    }
+});
+
+
+
 // ********************* End Events *********************
 
 // ********************* Main *********************
